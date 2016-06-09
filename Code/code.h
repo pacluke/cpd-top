afd0767ffa4db68b004243055d26604731f5c66e
Lucas Flores <lucasflores77@gmail.com>
2016-06-09 19:42:24 -0300

    um parser bem merda

---
+++ Code/code.h
@@ -0,0 +1,17 @@
+//
+//  code.h
+//  trabalho cpd
+//
+//  Created by Lucas Flores on 6/9/16.
+//  Copyright © 2016 pacluke.com. All rights reserved.
+//
+
+#ifndef code_h
+#define code_h
+
+#include <stdio.h>
+
+void makePlaylist();
+
+
+#endif /* code_h */
---
+++ Code/main.c
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  trabalho cpd
+//
+//  Created by Lucas Flores on 6/9/16.
+//  Copyright © 2016 pacluke.com. All rights reserved.
+//
+
+#include <stdio.h>
+#include "code.h"
+
+int main() {
+
+    makePlaylist();
+    return 0;
+}
